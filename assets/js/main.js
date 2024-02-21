27a985f01f4d85a76c5a0fdd38f9b6a1d02c0c74
StefDev1801 <stefano200118@gmail.com>
2024-02-21 17:00:11 +0100

    fix milestone 2

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,15 +8,16 @@ const fizzbuzz = 'FizzBuzz'
 
 const ul = document.querySelector('ul.full_list')
 console.log(ul);
-const li = document.createElement('li');//mi permette semplicemente di aggiungere un li figo
 //posso usarlo per dargli una classe così da poterlo far diventare un quadrato tramite css
-console.log(li);
+
 
 //const elementAdd = document.querySelector('.full_list')
-
+//const textnode = document.createTextNode
 
 
 for (let i = 1; i <= 100; i++){
+const li = document.createElement('li');//mi permette semplicemente di aggiungere un li figo
+
     //console.log(i); //just for checking
     if (i % 3 == 0) {
         if (i % 5 == 0) {
@@ -24,13 +25,13 @@ for (let i = 1; i <= 100; i++){
         //elementAdd.innerHTML +=`<li>${fizzbuzz}</li> ` //not required
             li.append(fizzbuzz); //questo è quello che si vedrà dentro il mio list item
             //devo far sì che per ogni fizzbuzz che ho aggiungo un li
-            ul.append(li);//qui errore
+
             
         }else{
             console.log(fizz); //first milestone
         //elementAdd.innerHTML +=`<li>${fizz}</li> `  //not required
             li.append(fizz);
-            ul.append(li);//qui errore
+
 
             
         } //end if inside
@@ -38,14 +39,15 @@ for (let i = 1; i <= 100; i++){
         console.log(buzz); //first milestone
         //elementAdd.innerHTML +=`<li>${buzz}</li> `  //not required
         li.append(buzz);
-        ul.append(li);//qui errore
+
 
         
     }else{
         console.log(i); //first milestone
         //elementAdd.innerHTML +=`<li>${i}</li> `  //not required
         li.append(i);
-        ul.append(li);//qui errore
         
     }
+    ul.append(li);//ne metto uno che viene letto a fine ciclo
+
 }
