5240ed248b52014f26766ce89d3ced8bdd557daf
StefDev1801 <stefano200118@gmail.com>
2024-02-21 16:40:09 +0100

    problem with append

--- assets/js/main.js
+++ assets/js/main.js
@@ -12,7 +12,7 @@ const li = document.createElement('li');//mi permette semplicemente di aggiunger
 //posso usarlo per dargli una classe così da poterlo far diventare un quadrato tramite css
 console.log(li);
 
-const elementAdd = document.querySelector('.full_list')
+//const elementAdd = document.querySelector('.full_list')
 
 
 
@@ -21,22 +21,31 @@ for (let i = 1; i <= 100; i++){
     if (i % 3 == 0) {
         if (i % 5 == 0) {
             console.log(fizzbuzz); //first milestone
-        elementAdd.innerHTML +=`<li>${fizzbuzz}</li> ` 
-
+        //elementAdd.innerHTML +=`<li>${fizzbuzz}</li> ` //not required
+            li.append(fizzbuzz); //questo è quello che si vedrà dentro il mio list item
+            //devo far sì che per ogni fizzbuzz che ho aggiungo un li
+            ul.append(li);//qui errore
             
         }else{
             console.log(fizz); //first milestone
-        elementAdd.innerHTML +=`<li>${fizz}</li> ` 
+        //elementAdd.innerHTML +=`<li>${fizz}</li> `  //not required
+            li.append(fizz);
+            ul.append(li);//qui errore
 
             
         } //end if inside
     }else if(i % 5 == 0){
         console.log(buzz); //first milestone
-        elementAdd.innerHTML +=`<li>${buzz}</li> ` 
+        //elementAdd.innerHTML +=`<li>${buzz}</li> `  //not required
+        li.append(buzz);
+        ul.append(li);//qui errore
 
         
     }else{
         console.log(i); //first milestone
-        elementAdd.innerHTML +=`<li>${i}</li> ` 
+        //elementAdd.innerHTML +=`<li>${i}</li> `  //not required
+        li.append(i);
+        ul.append(li);//qui errore
+        
     }
 }
