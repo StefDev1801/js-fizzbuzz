e3055632cad6580fcf440100bb3673feff5e0bc2
StefDev1801 <stefano200118@gmail.com>
2024-02-21 16:01:17 +0100

    add more or less second milestone, need to fix

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,17 +4,38 @@ const fizz = 'Fizz';
 const buzz = 'Buzz';
 //console.log(buzz);
 const fizzbuzz = 'FizzBuzz'
+//console.log(FizzBuzz);
+
+const ul = document.querySelector('ul.full_list')
+console.log(ul);
+const li = document.createElement('li');//mi permette semplicemente di aggiungere un li figo
+console.log(li);
+
+li.append('hola')
+ul.append(li);
+
+
+
+
 for (let i = 1; i <= 100; i++){
-    //console.log(i);
+    //console.log(i); //just for checking
     if (i % 3 == 0) {
         if (i % 5 == 0) {
-            console.log(fizzbuzz);            
+            console.log(fizzbuzz); //first milestone
+            li.append(fizzbuzz)
+            ul.append(li);
         }else{
-            console.log(fizz);
+            console.log(fizz); //first milestone
+            li.append(fizz)
+            ul.append(li);
         } //end if inside
     }else if(i % 5 == 0){
-        console.log(buzz);
+        console.log(buzz); //first milestone
+        li.append(buzz)
+        ul.append(li);
     }else{
-        console.log(i);
+        console.log(i); //first milestone
+        li.append(i)
+        ul.append(li);
     }
 }
